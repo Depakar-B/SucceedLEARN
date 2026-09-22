a28d79939664070683b06a26dcfa5ea46335801b
Shrikar H M <shrikar@succeedtech.com>
2026-09-22 16:16:59 +0530

    Reapply "Merge branch 'hotfix/s-metrics'"
    This reverts commit e87e36e87e37e13496471c5ac2c456a4a8f4f275.

--- wp-content/themes/akaza-adventure/inc/bootstrap-pages.php
+++ wp-content/themes/akaza-adventure/inc/bootstrap-pages.php
@@ -134,6 +134,11 @@ function akaza_bootstrap_pages() {
 			'title'    => 'S-Bytes',
 			'template' => 'page-templates/s-bytes.php',
 		),
+		array(
+			'slug'     => 's-metrics',
+			'title'    => 'S-Metrics',
+			'template' => 'page-templates/s-metrics-tracking-reporting.php',
+		),
 	);
 
 	$created_page = false;
